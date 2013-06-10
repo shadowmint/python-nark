dda7a93c148187f26c224d7d33892eb297f4762a
Doug <linderd@iinet.net.au>
2013-06-10 17:06:18 +0800

    fixed implements to work in all

--- src/nark/implements.py
+++ src/nark/implements.py
@@ -1,22 +1,22 @@
 import inspect
+import sys
+
 
 class ImplementsException(Exception):
   def __init__(self, msg, signature):
     super(ImplementsException, self).__init__()
     self.signature = signature
 
+
 def implements(*T):
   def inner(cls):
     cls.__identity = []
     for t in T:
-      print("Inner class? %s" % str(t))
-      t_methods = inspect.getmembers(t, predicate=inspect.isfunction)
-      print("Methods: %r" % t_methods)
-      c_methods = inspect.getmembers(cls, predicate=inspect.isfunction)
+      t_methods = inspect.getmembers(t, predicate=lambda x: inspect.isfunction(x) or inspect.ismethod(x))
+      c_methods = inspect.getmembers(cls, predicate=lambda x: inspect.isfunction(x) or inspect.ismethod(x))
       sig = {}
       for i in t_methods:
         name = i[0]
-        print("Found: %s" % name)
         if name[:2] != "__":
           sig[name] = False
       for i in c_methods:
